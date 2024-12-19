27d9c15cbd770c61705db558ad44eea7813ca970
c4nk_ <89845830+c4nkerm3me@users.noreply.github.com>
2024-12-19 17:10:49 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ var Config = {};
  * What should the text in the center of the screen be?
  * if empty it will fill in your Server Name
  */
-Config.title = "";
+Config.title = "homeless grad";
 
 /**
  * Enable map text in the top left corner of the screen?
@@ -27,8 +27,9 @@ Config.enableAnnouncements = true;
  * only works if enableAnnouncements = true
  */
 Config.announceMessages = [
-  "Enjoy your stay!",
-  "This loading screen is made by Exsite."
+  "for the homeless",
+  "this is a shit server",
+  "non-p2w homigrad ftw",
 ];
 
 /**
